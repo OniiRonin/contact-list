56b9407f2c61bc871ed7a5313069235dd9379309
Jean Figueroa <jankrlosf@gmail.com>
2023-03-13 13:01:27 -0500

    se realiza el ejercicio solicitado

--- contact-list.js
+++ contact-list.js
@@ -1,1 +1,27 @@
 //contact-list
+const listaContactos = [
+    "Danna Cortes",
+    "John Castro",
+    "Maria Donado",
+    "Luis Lombo"
+];
+
+function añadirContacto(nombreApellido) {
+    listaContactos.push(nombreApellido);
+}
+
+añadirContacto("Jean Figueroa");
+function borrarContacto(nombreApellido) {
+    const indice = listaContactos.indexOf(nombreApellido);
+    if (indice > -1) {
+        listaContactos.splice(indice, 1);
+    }
+}
+
+borrarContacto("Maria Donado");
+function imprimirContactos() {
+    console.log("Lista de contactos:");
+    listaContactos.forEach(contacto => console.log(contacto));
+}
+
+imprimirContactos();
